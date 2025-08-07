846e4081162614c50fe21f2025679cade1160540
mahmoud-bayoumi <mahmoudbayoumi110@gmail.com>
2025-08-07 14:44:47 +0300

    create ecommerce view appbar

--- lib/views/ecommerce_view.dart
+++ lib/views/ecommerce_view.dart
@@ -6,6 +6,28 @@ class EcommerceView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(
+              Icons.search,
+              color: Colors.grey,
+            ),
+            SizedBox(
+              width: 12,
+            ),
+            Text(
+              'Search keywords..',
+              style: TextStyle(color: Colors.grey, fontSize: 18),
+            ),
+            Spacer(),
+            Icon(
+              Icons.menu,
+              color: Colors.grey,
+            ),
+          ],
+        ),
+      ),
       body: Placeholder(),
     );
   }
